5b9e85aae536d171191a3807559d1dd79d4f868e
aponsero <alise.ponsero@gmail.com>
2022-05-29 15:22:55 +0300

    Delete config.sh

--- config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-# INPUT LIST and  DIRECTORY
-IN_DIR="test_files" # Input directory with samples to process
-OUT_DIR="test_output" # Output directory where to save the diamond output
-IN_LIST="list_to_process.txt" # DO NOT MODIFY
-# Diamond configurations
-DB="test_files/test_database.dmnd" # custom or non-custom database to use
-MAX_TAR=5 # max targets match per query
- 
